ca7b8f7a6dfd9f42b86bf6f22ab25ff48d445d0a
panosntaveas <63146477+panosntaveas@users.noreply.github.com>
2020-04-11 17:23:46 +0300

    realloc of library done/ extra levels added in lib

--- riddle.cpp
+++ riddle.cpp
@@ -7,16 +7,18 @@
 #include <windows.h>
 #define width 20
 #define height 20
-#define initial_speed 400
-#define arr_size 3
 #define  MAX_LEN  100
+#define arr_size 3
 
 struct index{
 	char q[MAX_LEN];
 	char a[MAX_LEN];
-}library[arr_size];
+	int size;
+};
 
-//typedef struct index index;
+typedef struct index index;
+struct index *library;//=(index*)malloc(sizeof(index));
+
 void remove_newline(char str[]){
 	int i=0;
 	while(str[i]!='\0'){
@@ -32,19 +34,26 @@ void open_file(){
 	char file[20]="library.txt";
 	fp=fopen(file,"r");
     if (fp==NULL) {
-    //  fprintf(stderr,"Impossible to open the file %s\n\n",file);
       exit(1);
     }
     int i=0;
-
+    int size=1;
     while(!feof(fp) ){
-    	fgets(library[i].q,MAX_LEN,fp);
-    	fgets(library[i].a,MAX_LEN,fp);
-    	remove_newline(library[i].q);
-    	remove_newline(library[i].a);
-    //	printf("%s ,%s\n",library[i].q,library[i].a);
+    	index stream;
+    	library=(index*) realloc(library,sizeof(index) * size);
+    	fgets(stream.q,MAX_LEN,fp);
+    	fgets(stream.a,MAX_LEN,fp);
+    	if(feof(fp)){
+    		break;
+    	}
+    	remove_newline(stream.q);
+    	remove_newline(stream.a);
+    	library[i]=stream;
+    	library->size=size;
     	i++;
+    	size++;
     }
+    fclose(fp);
 }
 
 void Gotoxy(int x, int y) {
@@ -153,7 +162,7 @@ current currentlevel(current cur,int level){
 }
 
 void play(){
-	open_file();
+//	open_file();
 	int level=0;
 	int result;
 	do{
@@ -162,6 +171,8 @@ void play(){
 			print(cur.len_q,cur.len_a,cur.index.q,level);
 			printf("\n    Enter to solve");
 			printf("\n    ESC to exit");
+			Gotoxy(width+3,height/2);
+			printf("O");
 			result=1;
 			int i=0;
 			char input [cur.len_a];
@@ -195,7 +206,7 @@ void play(){
 				}
 				if(c==27){
 					system("cls");
-					exit(0);
+					return;
 				}
 			}
 			input[i]='\0';
@@ -207,78 +218,13 @@ void play(){
             Gotoxy(0,0);
         }while(result!=0);
         Gotoxy(0,0);
-    }while(level<arr_size);
+    }while(level<library->size);
 }
 
-/*void play(){
-	char ans[arr_size][20] = {"mars","2","hamlet"};
-	char quest[arr_size][20]={"* oooOoooo","1+1","2b || !2b"};
-	int q_size[arr_size];
-	int a_size[arr_size];
-	int i=0;
-	for(i=0; i<arr_size; i++){
-		a_size[i]=strlen(ans[i]);
-		q_size[i]=strlen(quest[i]);
-	}
-	int level=0;
-	int result;
-	do{
-		char cur_quest[q_size[level]];
-		strcpy(cur_quest,quest[level]);
-		char cur[a_size[level]];
-		strcpy(cur,ans[level]);
-		do{
-			print(q_size[level],a_size[level],cur_quest,level);
-			printf("\n    ESC to exit");
-			result=1;
-			int i=0;
-			char input [a_size[level]];
-			int x=width/2 - a_size[level]/2;
-			int y=12;
-			Gotoxy(x,y);
-			int pos=x;
-			char c;
-			while(i<a_size[level]+1){
-				c=getch();
-		
-				if(c=='\r' || pos==x-1 ){
-					break;
-				}
-				else if(c=='\b' && pos==x){
-					break;
-				}
-				else if(c!='\b' && c!='\r' && c!='\n'){
-					input[i]=c;
-					printf("%c",input[i]);
-					pos++;
-						i++;
-				}
-				else if (c == '\b'){
-					i--;
-					input[i]='*';
-					pos--;
-					Gotoxy(pos,y);
-					printf("%c",input[i]);
-	                Gotoxy(pos,y);
-					input[i]='\0';
-				}
-				
-				
-			}
-			input[i]='\0';
-			result =strcmp(input,cur);
-			if(result==0){
-				level++;
-				break;
-			}
-            Gotoxy(0,0);
-        }while(result!=0);
-        Gotoxy(0,0);
-    }while(level<arr_size);
-}*/
-
 int main(){
-//	open_file();
+	open_file();
 	play();
+	system("cls");
+	free(library);
 	return 0;
 }
